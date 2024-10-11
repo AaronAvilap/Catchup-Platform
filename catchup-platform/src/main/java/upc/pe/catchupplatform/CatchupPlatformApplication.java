83d6a497ce789cd4aa323c988690948fe919b703
Aarón Alexander Avila Palacios <102324692+AaronAvilap@users.noreply.github.com>
2024-10-10 20:49:31 -0500

    Add files via upload

---
+++ catchup-platform/src/main/java/upc/pe/catchupplatform/CatchupPlatformApplication.java
@@ -0,0 +1,13 @@
+package upc.pe.catchupplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchupPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchupPlatformApplication.class, args);
+	}
+
+}
---
+++ catchup-platform/src/test/java/upc/pe/catchupplatform/CatchupPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package upc.pe.catchupplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchupPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
